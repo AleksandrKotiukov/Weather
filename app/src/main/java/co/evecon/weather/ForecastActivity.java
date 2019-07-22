5fd03c9a438b7b6d571808c1072a6bc6a4f04a9b
Borg <alex.kotyukov@yandex.ru>
2019-07-22 18:15:18 +0300

    Add data on the weather forecast screen

--- app/src/main/java/co/evecon/weather/ForecastActivity.java
+++ app/src/main/java/co/evecon/weather/ForecastActivity.java
@@ -3,11 +3,18 @@ package co.evecon.weather;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ForecastActivity extends AppCompatActivity {
     private TextView cityName;
     private String sendCityName;
+    private int showTemperature;
+    private int showPressure;
+    private int showHumidity;
+    private int showWindSpeed;
+    private LinearLayout weatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +22,43 @@ public class ForecastActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         cityName = findViewById(R.id.cityName);
+        weatherData = findViewById(R.id.weatherData);
 
         Intent intent = getIntent();
 
         sendCityName = intent.getStringExtra("cityName");
-
         cityName.setText(sendCityName);
+
+        showTemperature = intent.getIntExtra("showTemperature", 2);
+        if (showTemperature == 1) {
+            TextView textView = new TextView(this);
+            textView.setText(getResources().getString(R.string.temperature));
+            textView.setTextSize(15);
+            weatherData.addView(textView);
+        }
+
+        showPressure = intent.getIntExtra("showPressure", 2);
+        if (showPressure == 1) {
+            TextView textView = new TextView(this);
+            textView.setText(getResources().getString(R.string.pressure));
+            textView.setTextSize(15);
+            weatherData.addView(textView);
+        }
+
+        showHumidity = intent.getIntExtra("showHumidity", 2);
+        if (showHumidity == 1) {
+            TextView textView = new TextView(this);
+            textView.setText(getResources().getString(R.string.humidity));
+            textView.setTextSize(15);
+            weatherData.addView(textView);
+        }
+
+        showWindSpeed = intent.getIntExtra("showWindSpeed", 2);
+        if (showWindSpeed == 1) {
+            TextView textView = new TextView(this);
+            textView.setText(getResources().getString(R.string.windSpeed));
+            textView.setTextSize(15);
+            weatherData.addView(textView);
+        }
     }
 }
--- app/src/main/java/co/evecon/weather/MainActivity.java
+++ app/src/main/java/co/evecon/weather/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     private Button forecastButton;
     private EditText cityName;
     private String enteredCityName;
+    private CheckBox showTemperature;
+    private CheckBox showPressure;
+    private CheckBox showHumidity;
+    private CheckBox showWindSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +25,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cityName = findViewById(R.id.city);
-
+        showTemperature = findViewById(R.id.checkBoxTemperature);
+        showPressure = findViewById(R.id.checkBoxPressure);
+        showHumidity = findViewById(R.id.checkBoxHumidity);
+        showWindSpeed = findViewById(R.id.checkBoxWindSpeed);
         forecastButton = findViewById(R.id.forecastButton);
+
         forecastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Enter city", Toast.LENGTH_SHORT).show();
                 } else {
                     Intent intent = new Intent(MainActivity.this, ForecastActivity.class);
+                    if (showTemperature.isChecked()) {
+                        intent.putExtra("showTemperature", 1);
+                    } else {
+                        intent.putExtra("showTemperature", 0);
+                    }
+                    if (showPressure.isChecked()) {
+                        intent.putExtra("showPressure", 1);
+                    } else {
+                        intent.putExtra("showPressure", 0);
+                    }
+                    if (showHumidity.isChecked()) {
+                        intent.putExtra("showHumidity", 1);
+                    } else {
+                        intent.putExtra("showHumidity", 0);
+                    }
+                    if (showWindSpeed.isChecked()) {
+                        intent.putExtra("showWindSpeed", 1);
+                    } else {
+                        intent.putExtra("showWindSpeed", 0);
+                    }
                     intent.putExtra("cityName", enteredCityName);
                     startActivity(intent);
                 }
