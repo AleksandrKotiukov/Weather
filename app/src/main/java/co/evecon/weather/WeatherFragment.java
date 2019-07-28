1f7ac247068703457eef01ac417f98e43f693ea1
Borg <alex.kotyukov@yandex.ru>
2019-07-28 11:30:15 +0300

    Add logic to weather fragment

--- app/src/main/java/co/evecon/weather/WeatherFragment.java
+++ app/src/main/java/co/evecon/weather/WeatherFragment.java
@@ -1,18 +1,23 @@
 package co.evecon.weather;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 public class WeatherFragment extends Fragment {
 
     private TextView cityName;
+    private LinearLayout weatherData;
+    private Button sendEmail;
     private MainActivity mainActivity;
 
     @Override
@@ -21,9 +26,48 @@ public class WeatherFragment extends Fragment {
         // Inflate the layout for this fragment
         View fragmentView = inflater.inflate(R.layout.fragment_weather, container, false);
 
-        cityName = fragmentView.findViewById(R.id.cityWeatherFragmentName);
         mainActivity = (MainActivity) getActivity();
-        cityName.setText(mainActivity.enteredCityName);
+        cityName = fragmentView.findViewById(R.id.cityName);
+        weatherData = fragmentView.findViewById(R.id.weatherData);
+        sendEmail = fragmentView.findViewById(R.id.forecastEmailButton);
+
+        cityName.setText(mainActivity.getEnteredCityName());
+
+        if (mainActivity.getShowTemperature()) {
+            TextView textView = new TextView(getActivity());
+            textView.setText(getResources().getString(R.string.temperature));
+            weatherData.addView(textView);
+        }
+
+        if (mainActivity.getShowPressure()) {
+            TextView textView = new TextView(getActivity());
+            textView.setText(getResources().getString(R.string.pressure));
+            weatherData.addView(textView);
+        }
+
+        if (mainActivity.getShowHumidity()) {
+            TextView textView = new TextView(getActivity());
+            textView.setText(getResources().getString(R.string.humidity));
+            weatherData.addView(textView);
+        }
+
+        if (mainActivity.getShowWindSpeed()) {
+            TextView textView = new TextView(getActivity());
+            textView.setText(getResources().getString(R.string.windSpeed));
+            weatherData.addView(textView);
+        }
+
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/html");
+                intent.putExtra(Intent.EXTRA_EMAIL, "emailaddress@emailaddress.com");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+                intent.putExtra(Intent.EXTRA_TEXT, "I'm email body.");
+                startActivity(Intent.createChooser(intent, "Send Email"));
+            }
+        });
 
         return fragmentView;
     }
