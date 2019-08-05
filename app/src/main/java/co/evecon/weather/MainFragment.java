cc6fb8bf099aaca182fececaa91ec9a526aeeb6e
Borg <alex.kotyukov@yandex.ru>
2019-08-05 10:07:15 +0300

    Add edit text material design

--- app/src/main/java/co/evecon/weather/MainFragment.java
+++ app/src/main/java/co/evecon/weather/MainFragment.java
@@ -1,19 +1,17 @@
 package co.evecon.weather;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.Toast;
-
 
 public class MainFragment extends Fragment {
 
-    private EditText cityName;
     private String enteredCityName;
     private CheckBox showTemperature;
     private CheckBox showPressure;
@@ -22,6 +20,7 @@ public class MainFragment extends Fragment {
     private Button forecastButton;
     private WeatherFragment weatherFragment;
     private MainActivity mainActivity;
+    private TextInputEditText inputCityName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,19 +28,19 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        cityName = fragmentView.findViewById(R.id.city);
         showTemperature = fragmentView.findViewById(R.id.checkBoxTemperature);
         showPressure = fragmentView.findViewById(R.id.checkBoxPressure);
         showHumidity = fragmentView.findViewById(R.id.checkBoxHumidity);
         showWindSpeed = fragmentView.findViewById(R.id.checkBoxWindSpeed);
         forecastButton = fragmentView.findViewById(R.id.forecastButton);
+        inputCityName = fragmentView.findViewById(R.id.inputCityName);
         weatherFragment = new WeatherFragment();
         mainActivity = (MainActivity) getActivity();
 
         forecastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enteredCityName = cityName.getText().toString();
+                enteredCityName = inputCityName.getText().toString();
                 if (enteredCityName.equals("")) {
                     Toast.makeText(getActivity(), "Enter city", Toast.LENGTH_SHORT).show();
                 } else {
